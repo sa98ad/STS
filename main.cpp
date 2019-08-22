0199a9fa970ec668277666c38a0ea62158eb31f6
sa98ad <54397979+sa98ad@users.noreply.github.com>
2019-08-22 15:34:55 +0530

    added printing content

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
-  cout<<"Hello World"<<endl;
+ int choice=0;
+ while(true) 
+ {
+    cout << "******WELCOME TO SMART TRAVEL SERVICES****** \n \n";
+    cout << "Enter your Choice \n";
+    cout << "1. Change Configuration\n2.Find Optimal Route\n3.Exit\n";
+    cin >> choice;
+    switch(choice)
+    {
+        case 1:
+        {
+            cout << "\nChange Configuration\n";
+            cout << "1.Set optimality\n2.Set Output Format";
+        }
+    }
+ }
 }
+
